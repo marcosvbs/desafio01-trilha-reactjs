8ffb2455c932b6fcd93e1a12769d4508bf6943fc
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2021-05-07 13:03:48 -0300

    added functionalities for adding, deleting and checking tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle != '') {
+
+      setTasks(tasks.concat({
+        id: Math.floor(Math.random() * 1000),
+        title: newTaskTitle,
+        isComplete: false
+      }));
+
+      setNewTaskTitle('');
+      
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => {
+
+      if ( task.id == id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+
+    })
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
